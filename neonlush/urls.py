3acb2fe6f8401049e5af3f5e04db5b6468bd83c0
selametsamli <selamet96@gmail.com>
2025-07-06 22:29:02 +0300

    Add URL routing for main app and include user API routes

--- neonlush/urls.py
+++ neonlush/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.neonlush_main.urls')),
     path('user', include('apps.user.urls')),
 ]
